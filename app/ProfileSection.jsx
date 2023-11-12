4e5e83d66868d7471cf456fcf8ffa53016281bc4
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2023-11-12 08:52:59 +0800

    update some image

--- app/ProfileSection.jsx
+++ app/ProfileSection.jsx
@@ -24,7 +24,16 @@ export default function ProfileSection() {
           <div className="left-section">
             <div className="flex justify-center">
               <div className="w-[300px] px-5 py-[20px] flex justify-center">
-                <div className="rounded-full border border-gray-600 shadow shadow-lg w-[150px] h-[150px] bg-[url('/static/img/mangwahyu.png')] bg-center bg-cover"></div>
+
+                  <ExportedImage
+                    src='/static/img/mangwahyu.png'
+                    alt="thumbnail-project"
+                    // bg-center bg-cover
+                    sizes="100%"
+                    width={150}
+                    height={150}
+                    className="rounded-full border border-gray-600 shadow-lg w-[150px] h-[150px]"
+                  />
               </div>
             </div>
             <hr className="border border-gray-500 my-3 border-[1px]" />
@@ -47,7 +56,7 @@ export default function ProfileSection() {
               <p className="text-white">Skills</p>;
               <div className="lg:grid lg:grid-cols-5 flex flex-wrap gap-3 lg:justify-normal justify-between items-center">
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     className="mt-[5px]"
                     src="/static/img/icon/github.png"
                     alt=""
@@ -56,10 +65,15 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-3 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage src="/static/img/icon/js.png" alt="" width={60} height={60} />
+                  <ExportedImage
+                    src="/static/img/icon/js.png"
+                    alt=""
+                    width={60}
+                    height={60}
+                  />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-2 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     src="/static/img/icon/vuejs.png"
                     alt=""
                     width={60}
@@ -67,7 +81,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-3 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     src="/static/img/icon/react.png"
                     alt=""
                     width={60}
@@ -75,7 +89,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-3 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     src="/static/img/icon/nuxt.webp"
                     alt=""
                     width={60}
@@ -83,7 +97,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     className="mt-[2px]"
                     src="/static/img/icon/next.png"
                     alt=""
@@ -92,7 +106,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 p-2 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     className="mt-[1px]"
                     src="/static/img/icon/node.png"
                     alt=""
@@ -101,7 +115,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-1 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     className="mt-[2px]"
                     src="/static/img/icon/php.png"
                     alt=""
@@ -110,7 +124,7 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-3 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     src="/static/img/icon/laravel.png"
                     alt=""
                     width={60}
@@ -118,14 +132,13 @@ export default function ProfileSection() {
                   />
                 </div>
                 <div className="cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-110 dark:border-gray-700 dark:bg-gray-800 w-[45px] h-[45px]  p-3 bg-gray-600 rounded-lg bg-blue-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                 <ExportedImage
+                  <ExportedImage
                     src="/static/img/icon/flutter.png"
                     alt=""
                     width={60}
                     height={60}
                   />
                 </div>
-                
               </div>
             </div>
           </div>
